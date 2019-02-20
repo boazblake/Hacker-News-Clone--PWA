69453c7751fd705a05af558c6ada04bcbfc1bcb4
Boaz Blake <boazblake@gmail.com>
2019-02-20 05:48:58 -0600

    components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Layout from './Layout.js'
 const isEmpty = data => data.length == 0
 
 const loadData = model => url => route =>
-  m.request({ url, method: 'GET' }).then(data => {
-    model.data[route] = data
-  })
+  m
+    .request({
+      url,
+      method: 'GET',
+    })
+    .then(data => {
+      model.data[route] = data
+    })
 
 const getData = model => path => {
   model.state.route = path
@@ -15,21 +20,15 @@ const getData = model => path => {
   return model
 }
 
-const postVM = ({ title, body }) => ({ title, body })
-
-const toVm = (type, item) => {
-  console.log('item', type)
-  switch (type) {
-    case '/posts':
-      postVM(item)
-      break
-  }
-}
-
-const Item = ({ attrs }) => {
+const Posts = ({
+  attrs: {
+    item: { title, body },
+  },
+}) => {
+  console.log(title, body)
   return {
-    view: () => {
-      return m(
+    view: () =>
+      m(
         '',
         {
           style: {
@@ -40,16 +39,155 @@ const Item = ({ attrs }) => {
             height: '150px',
           },
         },
-        'item'
-      )
-    },
+        [m('h1', title), m('p', body)]
+      ),
+  }
+}
+
+const Comments = ({
+  attrs: {
+    item: { email, name, body },
+  },
+}) => {
+  return {
+    view: () =>
+      m(
+        '',
+        {
+          style: {
+            flexGrow: 1,
+            margin: '10px',
+            backgroundColor: 'rgba(41,18,185 ,.2)',
+            width: '150px',
+            height: '150px',
+          },
+        },
+        [m('h1', name), m('p', email), m('p', body)]
+      ),
+  }
+}
+
+const Albums = ({
+  attrs: {
+    item: { title },
+  },
+}) => {
+  return {
+    view: () =>
+      m(
+        '',
+        {
+          style: {
+            flexGrow: 1,
+            margin: '10px',
+            backgroundColor: 'rgba(41,18,185 ,.2)',
+            width: '150px',
+            height: '150px',
+          },
+        },
+        [m('h1', title)]
+      ),
+  }
+}
+
+const Photos = ({
+  attrs: {
+    item: { email, name, body },
+  },
+}) => {
+  return {
+    view: () =>
+      m(
+        '',
+        {
+          style: {
+            flexGrow: 1,
+            margin: '10px',
+            backgroundColor: 'rgba(41,18,185 ,.2)',
+            width: '150px',
+            height: '150px',
+          },
+        },
+        [m('h1', name), m('p', email), m('p', body)]
+      ),
+  }
+}
+
+const Todos = ({
+  attrs: {
+    item: { completed, title },
+  },
+}) => {
+  return {
+    view: () =>
+      m(
+        '',
+        {
+          style: {
+            flexGrow: 1,
+            margin: '10px',
+            backgroundColor: 'rgba(41,18,185 ,.2)',
+            width: '150px',
+            height: '150px',
+          },
+        },
+        [
+          m('h1', title),
+          m(
+            'input[type=checkbox]',
+            { onclick: () => (completed = !completed), checked: completed },
+            'Done'
+          ),
+        ]
+      ),
+  }
+}
+
+const Users = ({
+  attrs: {
+    item: { email, name, body },
+  },
+}) => {
+  return {
+    view: () =>
+      m(
+        '',
+        {
+          style: {
+            flexGrow: 1,
+            margin: '10px',
+            backgroundColor: 'rgba(41,18,185 ,.2)',
+            width: '150px',
+            height: '150px',
+          },
+        },
+        [m('h1', name), m('p', email), m('p', body)]
+      ),
+  }
+}
+
+const toComponent = type => {
+  switch (type) {
+  case '/posts':
+    return Posts
+  case '/comments':
+    return Comments
+  case '/albums':
+    return Albums
+  case '/photos':
+    return Photos
+  case '/todos':
+    return Todos
+  case '/users':
+    return Users
   }
 }
 
 const Container = () => {
   return {
-    view: ({ attrs: { model } }) =>
-      m(
+    view: ({ attrs: { model } }) => {
+      let Component = toComponent(model.state.route)
+      return m(
         'section.container',
         {
           style: {
@@ -64,12 +202,13 @@ const Container = () => {
           },
         },
         model.data[model.state.route].map((item, idx) => {
-          let dto = toVm(model.state.route, item)
-          console.log('dto', dto)
-
-          return m(Item, { key: idx, item: dto })
+          return m(Component, {
+            key: idx,
+            item: item,
+          })
         })
-      ),
+      )
+    },
   }
 }
 
@@ -77,29 +216,82 @@ export const App = model => {
   return {
     '/posts': {
       onmatch: (_, path) => getData(model)(path),
-      render: () => m(Layout, { model }, m(Container, { model })),
+      render: () =>
+        m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        ),
     },
     '/comments': {
       onmatch: (_, path) => getData(model)(path),
-      render: () => m(Layout, { model }, m(Container, { model })),
+      render: () =>
+        m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        ),
     },
     '/albums': {
       onmatch: (_, path) => getData(model)(path),
-      render: () => m(Layout, { model }, m(Container, { model })),
+      render: () =>
+        m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        ),
     },
     '/photos': {
       onmatch: (_, path) => getData(model)(path),
-      render: () => m(Layout, { model }, m(Container, { model })),
+      render: () =>
+        m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        ),
     },
     '/todos': {
       onmatch: (_, path) => getData(model)(path),
       render: () => {
-        return m(Layout, { model }, m(Container, { model }))
+        return m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        )
       },
     },
     '/users': {
       onmatch: (_, path) => getData(model)(path),
-      render: () => m(Layout, { model }, m(Container, { model })),
+      render: () =>
+        m(
+          Layout,
+          {
+            model,
+          },
+          m(Container, {
+            model,
+          })
+        ),
     },
   }
 }
