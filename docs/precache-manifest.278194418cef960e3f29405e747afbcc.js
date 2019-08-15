b4fe3f22fd19a3aba6674ddd91097f8a2867f0ca
bblake <bblake@goempyrean.com>
2019-08-15 11:40:09 -0500

    build

---
+++ docs/precache-manifest.278194418cef960e3f29405e747afbcc.js
@@ -0,0 +1,44 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "css/main-f84fb2d81480acc75f25.css"
+  },
+  {
+    "url": "css/main-f84fb2d81480acc75f25.css.gz"
+  },
+  {
+    "revision": "449a978b5aac92d1ef96b303b1ce6178",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "86ca78aff0717114e3ca190d49ebe008",
+    "url": "icons.js"
+  },
+  {
+    "revision": "7e2872fd711b9c9994c98449aa25d08c",
+    "url": "index.html"
+  },
+  {
+    "revision": "700d02be0213a412c810",
+    "url": "main-chunk.js"
+  },
+  {
+    "revision": "af4a5d1d41c5ee3e0c38f6de65188a13",
+    "url": "main-chunk.js.gz"
+  },
+  {
+    "revision": "464f89907f8fa3f77804afcae7efdbc8",
+    "url": "mithril.svg"
+  },
+  {
+    "revision": "993265aab6196272cfe7",
+    "url": "runtime-bundle.js"
+  },
+  {
+    "revision": "09e2deea4a94afc61272",
+    "url": "vendors-chunk.js"
+  },
+  {
+    "revision": "ae7936020905bb414ed26f1fb6b0d833",
+    "url": "vendors-chunk.js.gz"
+  }
+]);
